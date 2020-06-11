afd46df22150c38c975497688da7eee1dfc6465e
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-06-11 07:59:32 +0530

    added three more test cases

--- test/testGraph.js
+++ test/testGraph.js
@@ -5,4 +5,34 @@ describe('bfs', () => {
   it('should return false when the pair contains single node and it is not connected to itself', () => {
     assert.ok(!bfs([['a']], 'a', 'a'));
   });
+
+  it('should return true when the pair contains single node and it is connected to itself', () => {
+    assert.ok(bfs([['a', 'a']], 'a', 'a'));
+  });
+
+  it('should give false when the pairs contains two nodes and they are not connected', () => {
+    assert.ok(
+      !bfs(
+        [
+          ['a', 'c'],
+          ['b', 'c'],
+        ],
+        'a',
+        'b'
+      )
+    );
+  });
+
+  it('should give true when the pairs contains two nodes and they are perfectly connected', () => {
+    assert.ok(
+      bfs(
+        [
+          ['a', 'b'],
+          ['b', 'a'],
+        ],
+        'a',
+        'b'
+      )
+    );
+  });
 });
