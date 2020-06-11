0d55e04fd72f238d9e51e8be9efc365ed643cc5c
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-06-11 06:57:03 +0530

    added a function to convert the pairs into graph object

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,31 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const createGraph = function (graph, pair) {
+  if (pair[0] in graph) {
+    graph[pair[0]].push(pair[1]);
+    return graph;
+  }
+  graph[pair[0]] = (pair[1] && [pair[1]]) || [];
+  return graph;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  const graph = pairs.reduce(createGraph, {});
+};
+
+const main = function () {
+  const pairs = [
+    ['a', 'b'],
+    ['b', 'a'],
+    ['b', 'c'],
+  ];
+  const isExists = bfs(pairs, 'a', 'c');
+};
+
+main();
+
+module.exports = { bfs };
